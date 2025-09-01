36caee6e9be2f80492e90abaa5db321a96f8f357
GabrielLucas <gabriellucas3301@gmail.com>
2025-09-01 20:36:12 -0300

    chore: debugging

--- app/_lib/utils.ts
+++ app/_lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import "tailwindcss/tailwind.css"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
