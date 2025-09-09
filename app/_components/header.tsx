baf78e0b2535e74832cd7d76342c380127612b3b
GabrielLucas <gabriellucas3301@gmail.com>
2025-09-09 16:29:31 -0300

    feat:

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,19 +1,25 @@
 import { Button } from "./ui/button"
 import { Card, CardContent } from "./ui/card"
 import Image from "next/image"
-import { MenuIcon } from "lucide-react"
+import { MenuIcon, Sheet } from "lucide-react"
+import { SheetTrigger } from "./ui/sheet"
 
 const Header = () => {
   return (
     <Card>
-      <CardContent className="p-5 justify-between flex flex-row items-center">
+      <CardContent className="flex flex-row items-center justify-between p-5">
         <Image alt="FSW Barber" src="/logo.png" width={150} height={50} />
-        <Button size="icon" variant="black">
-        <MenuIcon />
-        </Button>
+
+        <Sheet>
+          <SheetTrigger>
+            <Button size="icon" variant="black">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+        </Sheet>
       </CardContent>
     </Card>
   )
 }
 
-export default Header    
+export default Header
